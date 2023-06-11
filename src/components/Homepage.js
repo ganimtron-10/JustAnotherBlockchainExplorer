45255d240f5b27258a6094acff8151d74d5feb0c
Praneeth Shetty <praneethshetty10@gmail.com>
2023-06-11 11:52:00 +0530

    fetching ethereum price in inr

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -1,8 +1,7 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import BlockInfo from './BlockInfo';
 import TransactionInfo from './TransactionInfo';
 import { Alchemy, Network } from 'alchemy-sdk';
-import { useEffect, useState } from 'react';
 
 const settings = {
     apiKey: process.env.REACT_APP_ALCHEMY_API_KEY,
@@ -11,27 +10,34 @@ const settings = {
 
 const alchemy = new Alchemy(settings);
 
-//   const [blockNumber, setBlockNumber] = useState();
-
-//   useEffect(() => {
-//     async function getBlockNumber() {
-//       setBlockNumber(await alchemy.core.getBlockNumber());
-//     }
-//     console.log("ue")
-//     getBlockNumber();
-//   });
-
-//   return <div className="App">Block Number: {blockNumber}</div>;
-
 const HomePage = () => {
+    const [ethPrice, setEthPrice] = useState(0);
 
+    useEffect(() => {
+        fetchEthPrice();
+
+    }, []);
+
+    function fetchEthPrice() {
+        fetch('https://api.coingecko.com/api/v3/coins/markets?vs_currency=inr&ids=ethereum')
+            .then((response) => response.json())
+            .then((data) => {
+                const price = data[0]?.current_price || 0;
+                setEthPrice(price);
+            })
+            .catch((error) => {
+                console.error('Error fetching Ethereum price:', error);
+            });
+    }
 
 
     return (
         <div className="bg-gray-900 min-h-screen">
             <nav className="bg-gray-700">
                 <div className="container mx-auto py-4 px-8">
-                    <h1 className="text-4xl font-bold text-white font-serif">Just Another Blockchain Explorer (JABE) </h1>
+                    <h1 className="text-4xl font-bold text-white font-serif">
+                        Just Another Blockchain Explorer (JABE)
+                    </h1>
                 </div>
             </nav>
 
@@ -39,7 +45,9 @@ const HomePage = () => {
                 <div className="container mx-auto px-4">
                     <div className="text-center mb-8">
                         <div className="bg-white rounded-lg p-4 inline-block">
-                            <p className="text-xl text-gray-900 font-serif">Current Price of Ethereum: $4000</p>
+                            <p className="text-xl text-gray-900 font-serif">
+                                Current Price of Ethereum: ₹{ethPrice} INR
+                            </p>
                         </div>
                     </div>
 
